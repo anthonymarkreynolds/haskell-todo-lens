6660b9bcc9cce7366ba279c416ad91d65e230199
Anthony <anthonymarkreynolds@outlook.com>
2024-03-07 05:32:53 +1000

    Refactor Todo data structure and add TodoList type

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,72 +16,87 @@ data Todo = Todo
   { _title      :: String
   , _completed  :: Bool
   , _created_at :: UTCTime
-  , _modified   :: UTCTime
+  , _modified   :: UTCTime 
   }
 makeLenses ''Todo
 
--- newtype TodoStore = TodoStore {
---   _todos :: [Todo]
--- }
--- makeLenses ''TodoStore
+data TodoList = TodoList
+  { _todos :: [Todo]
+  }
+makeLenses ''TodoList
 
 (<<) :: Monad m => m b -> m a -> m b
 (<<) = flip (>>)
 
-type App = LoggingT (StateT Todo IO) ()
+type App = LoggingT (StateT TodoList IO) ()
+ 
+-- type LensModifierWithAction a b = ASetter Todo Todo a b -> (a -> b) -> App
+-- type LensModifierWithValue  a b = ASetter Todo Todo a b -> b        -> App
 
-type LensModifierWithAction a b = ASetter Todo Todo a b -> (a -> b) -> App
-type LensModifierWithValue  a b = ASetter Todo Todo a b -> b        -> App
+-- modifyAndSetModified :: LensModifierWithAction a b
+-- modifyAndSetModified lens' f = do
+--   lens' %= f
+--   now <- liftIO getCurrentTime
+--   modified .= now
 
-modifyAndSetModified :: LensModifierWithAction a b
-modifyAndSetModified lens' f = do
-  lens' %= f
-  now <- liftIO getCurrentTime
-  modified .= now
+-- setAndSetModified :: LensModifierWithValue a b
+-- setAndSetModified lens' v = do
+--   lens' .= v
+--   now <- liftIO getCurrentTime
+--   modified .= now
 
-setAndSetModified :: LensModifierWithValue a b
-setAndSetModified lens' v = do
-  lens' .= v
-  now <- liftIO getCurrentTime
-  modified .= now
+-- (%:=) :: LensModifierWithAction a b
+-- (%:=) = modifyAndSetModified
 
-(%:=) :: LensModifierWithAction a b
-(%:=) = modifyAndSetModified
+-- (.:=) :: LensModifierWithValue a b
+-- (.:=) = setAndSetModified
 
-(.:=) :: LensModifierWithValue a b
-(.:=) = setAndSetModified
+showTodos :: [Todo] -> IO ()
+showTodos todo_list = putStr $ ifoldMap (\i todo 
+  -> show (i +1)
+  <> ". " 
+  <> (if todo ^. completed then "[x] " else "[ ] ")
+  <> (todo ^. title) <> "\n") todo_list
 
 test1 :: App
 test1 = do
-  -- Get the current title using the lens operator (^.)
-  $(logInfo) "getting title..."
-  currentTitle <- use title 
-  dateCreated <- use created_at
-  dateModified <- use modified
-  liftIO $ putStrLn $ "Current title: " ++ currentTitle 
-  liftIO $ putStrLn $ "Created at: " ++ show dateCreated
-  liftIO $ putStrLn $ "Modified at: " ++ show dateModified
-  -- Read a new title from the user
-  $(logInfo) "prompting user for title..." 
-  liftIO (putStrLn "Enter a new title:")
+  $(logInfo) "Start test1"
+  todo_list <- use todos
+  liftIO $ showTodos todo_list
 
-  $(logInfo) "getting title from user..."
-  newTitle <- liftIO getLine 
-  -- Update the title using the lens operator (.=)
-  $(logInfo) "setting title..."
-  title .:= newTitle
-  use modified >>= (\time ->liftIO $ putStrLn $ "Modified at: " ++ show time)
+-- test1 :: App
+-- test1 = do
+--   -- Get the current title using the lens operator (^.)
+--   $(logInfo) "getting title..."
+--   currentTitle <- use title 
+--   dateCreated <- use created_at
+--   dateModified <- use modified
+--   liftIO $ putStrLn $ "Current title: " ++ currentTitle 
+--   liftIO $ putStrLn $ "Created at: " ++ show dateCreated
+--   liftIO $ putStrLn $ "Modified at: " ++ show dateModified
+--   -- Read a new title from the user
+--   $(logInfo) "prompting user for title..." 
+--   liftIO (putStrLn "Enter a new title:")
 
-  use title >>= (\title' -> liftIO $ putStrLn ("Updated title: " ++ title'))
+--   $(logInfo) "getting title from user..."
+--   newTitle <- liftIO getLine 
+--   -- Update the title using the lens operator (.=)
+--   $(logInfo) "setting title..."
+--   title .:= newTitle
+--   use modified >>= (\time ->liftIO $ putStrLn $ "Modified at: " ++ show time)
+
+--   use title >>= (\title' -> liftIO $ putStrLn ("Updated title: " ++ title'))
 
 someFunc :: IO ()
 someFunc = do
   putStrLn "running test1..."
   currentTime <- getCurrentTime
-  evalStateT (runStderrLoggingT test1) $ Todo
-    { _title = "test"
-    , _completed = False
-    , _created_at = currentTime
-    , _modified = currentTime
-    }
+  evalStateT (runStderrLoggingT test1) $ TodoList [
+    Todo
+      { _title = "test"
+      , _completed = False
+      , _created_at = currentTime
+      , _modified = currentTime
+      }
+    ]
   putStrLn "done"
